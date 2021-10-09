28c51dd69005b6e09c5b5239f3bc7dd753a305da
InaWise <Blissful.simplicity16@gmail.com>
2021-10-09 15:23:37 -0400

    update

--- src/mock/data.js
+++ src/mock/data.js
@@ -18,10 +18,10 @@ export const heroData = {
 // ABOUT DATA
 export const aboutData = {
   img: 'AB2ACECD-FAA9-45FF-98B8-85858BED9DF8.jpeg',
-  paragraphOne: '',
-  paragraphTwo: '',
-  paragraphThree: '',
-  resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
+  paragraphOne: 'Front-End Web Developer leveraging my knowledge to build more intuitive user experiences on the web. Recently earned a certificate in Full Stack Development from the University of Central Florida and developed skills in Html, JavaScript, CSS, React.js, and responsive web design. Known as an innovative problem-solver passionate about developing apps, with a focus on mobile-first design and development. Aim for each project is to best engage the audience for an impactful user-experience.   Excited to leverage my skills in the future as part of a fast-paced, quality-driven team in order to build better experiences on the web.',
+  paragraphTwo: '  ',
+  paragraphThree: '   ',
+  resume: 'https://docs.google.com/document/d/1GQUmA-lUHfghLqYYOBQDMpTVupjaVaaYL5-lG29CgoI/edit?usp=sharing', // if no resume, the button will not show up
 };
 
 // PROJECTS DATA
@@ -29,8 +29,8 @@ export const projectsData = [
   {
     id: nanoid(),
     img: 'Horiseon (1).png',
-    title: '',
-    info: '',
+    title: 'Horiseon',
+    info: 'In this Challenge, a marketing agency has hired me to refactor an existing site to make it more accessible.',
     info2: '',
     url: '',
     repo: 'https://github.com/InaWise/Horiseon-code', // if no repo, the button will not show up
@@ -38,8 +38,8 @@ export const projectsData = [
   {
     id: nanoid(),
     img: 'iambored-home.png',
-    title: '',
-    info: '',
+    title: 'IAMBORED',
+    info: 'IAMBORED is an application that randomly chooses an event for you based off of the inputted city. It also allows you to choose a new event in that city. Once an interesting event is found the application provides a map of the location and any health warnings or information due to COVID-19.',
     info2: '',
     url: '',
     repo: 'https://github.com/InaWise/8.-Interactive-Front-End-Project', // if no repo, the button will not show up
@@ -47,8 +47,8 @@ export const projectsData = [
   {
     id: nanoid(),
     img: 'Note-Taker.png',
-    title: '',
-    info: '',
+    title: 'Note-taker',
+    info: 'This project is creates an application called Note Taker that can be used to write and save notes. This application will use an Express.js back end and will save and retrieve note data from a JSON file.',
     info2: '',
     url: '',
     repo: 'https://github.com/InaWise/11.-Express.js-Challenge-Note-Taker', // if no repo, the button will not show up
@@ -59,7 +59,7 @@ export const projectsData = [
 export const contactData = {
   cta: '',
   btn: '',
-  email: 'blissful.simplicity16@gmail.com',
+  email: 'inaawise@gmail.com',
 };
 
 // FOOTER DATA
